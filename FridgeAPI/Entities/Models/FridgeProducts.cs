95f95061c11290f50ac6116dfda535291016383b
NikitaL01 <lazovoy.nikita@gmail.com>
2022-08-16 10:42:26 +0300

    Adding Models.

---
+++ FridgeAPI/Entities/Models/FridgeProducts.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("fridge_products")]
+    public class FridgeProducts
+    {
+        [Column("id", TypeName = "UNIQUEIDENTIFIER")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("product_id", TypeName = "UNIQUEIDENTIFIER")]
+        [ForeignKey(nameof(Products))]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [Column("fridge_id", TypeName = "UNIQUEIDENTIFIER")]
+        [ForeignKey(nameof(Fridge))]
+        public Guid FridgeId { get; set; }
+
+        [Required]
+        [Column("quantity", TypeName = "int")]
+        public int Quantuty { get; set; }
+
+        public Fridge? Fridge { get; set; }
+
+        public Products? Products { get; set; }
+    }
+}
