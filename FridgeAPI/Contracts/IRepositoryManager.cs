eac31ce4f2bcdfa4c951258b9285845613bd1cb5
NikitaL01 <lazovoy.nikita@gmail.com>
2022-08-16 10:51:02 +0300

    Creating interfaces.

---
+++ FridgeAPI/Contracts/IRepositoryManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        IFridgeRepository Fridge { get; }
+
+        IFridgeModelRepository FridgeModel { get; }
+
+        IFridgeProductsRepository FridgeProducts { get; }
+
+        IProductsRepository Products { get; }
+
+        Task SaveAsync();
+    }
+}
