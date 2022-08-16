b7a41f07a96aa13c014f5ee15040bc2a29e29696
NikitaL01 <lazovoy.nikita@gmail.com>
2022-08-16 10:46:39 +0300

    Creating Data transfer objects.

---
+++ FridgeAPI/Entities/DataTransferObjects/ProductsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.DataTransferObjects
+{
+    public class ProductsDto
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int DefaultQuantity { get; set; }
+    }
+}
