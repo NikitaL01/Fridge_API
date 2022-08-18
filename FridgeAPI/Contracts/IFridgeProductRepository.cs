c2fa28ecc0b64e6ce7f75ca3c800cdecdbcbce03
NikitaL01 <73064686+NikitaL01@users.noreply.github.com>
2022-08-18 17:05:49 +0300

    Adding FridgeProductRepository.

---
+++ FridgeAPI/Contracts/IFridgeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+
+namespace Contracts
+{
+    public interface IFridgeProductRepository
+    {
+        Task<IEnumerable<FridgeProduct>> GetAllFridgeProductsAsync(bool trackChanges);
+
+        Task<FridgeProduct> GetFridgeProductAsync(Guid fridgeProductId, bool trackChanges);
+
+        void CreateFridgeProduct(FridgeProduct fridgeProduct);
+
+        Task<IEnumerable<FridgeProduct>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
+
+        void DeleteFridgeProduct(FridgeProduct fridgeProduct);
+    }
+}
---
+++ FridgeAPI/Repository/FridgeProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Entities.Context;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class FridgeProductRepository : RepositoryBase<FridgeProduct>, IFridgeProductRepository
+    {
+        public FridgeProductRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public async Task<IEnumerable<FridgeProduct>> GetAllFridgeProductsAsync(bool trackChanges) =>
+            await FindAll(trackChanges).ToListAsync();
+
+        public async Task<FridgeProduct> GetFridgeProductAsync(Guid fridgeProductId, bool trackChanges) =>
+            (await FindByCondition(fp => fp.Id.Equals(fridgeProductId), trackChanges)
+                .SingleOrDefaultAsync())!;
+
+        public void CreateFridgeProduct(FridgeProduct fridgeProduct) => Create(fridgeProduct);
+
+        public async Task<IEnumerable<FridgeProduct>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges) =>
+            await FindByCondition(fp => ids.Contains(fp.Id), trackChanges).ToListAsync();
+
+        public void DeleteFridgeProduct(FridgeProduct fridgeProduct) =>
+            Delete(fridgeProduct);
+    }
+}
