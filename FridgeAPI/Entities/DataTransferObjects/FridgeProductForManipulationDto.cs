0599daea0988a164aa552b9ad045010b1b9067a4
NikitaL01 <73064686+NikitaL01@users.noreply.github.com>
2022-08-19 13:13:52 +0300

    Adding Product and FridgeProduct dtos for creating and updating.

---
+++ FridgeAPI/Entities/DataTransferObjects/FridgeProductForManipulationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.DataTransferObjects;
+
+public class FridgeProductForManipulationDto
+{
+    [Required(ErrorMessage = "Product id is a required field.")]
+    public Guid ProductId { get; set; }
+
+    [Required(ErrorMessage = "Fridge id is a required field.")]
+    public Guid FridgeId { get; set; }
+
+    [Required(ErrorMessage = "Fridge product quantity is a required field.")]
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity is required and it can't be lower than 0")]
+    public int Quantity { get; set; }
+}
